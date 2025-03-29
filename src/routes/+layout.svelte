05f17c666ea47c27338f8e7f8bf7fbfffec70b46
Dokja <revanamike620@gmail.com>
2025-03-29 10:37:41 +0300

    Add layout components and global styles for SvelteKit application

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import Footer from '$lib/layout/footer.svelte';
+	import Header from '$lib/layout/header.svelte';
 	import '../app.css';
+	import '../scss/global.scss';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Header />
+<main>
+	{@render children()}
+</main>
+<Footer />
