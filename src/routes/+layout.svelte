0cb0377bcd1d2d70fd441addb6a27e32759b9ec6
Dokja <revanamike620@gmail.com>
2025-03-29 10:39:52 +0300

    Add Lato font and update global styles in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import Header from '$lib/layout/header.svelte';
 	import '../app.css';
 	import '../scss/global.scss';
+
+	import '@fontsource/lato/400.css';
+	import '@fontsource/lato/700.css';
+	import '@fontsource/lato/900.css';
 	let { children } = $props();
 </script>
 
