d45b653d6164b8c4037c24a9fad46ea55d2479b9
Dokja <revanamike620@gmail.com>
2025-03-29 10:50:21 +0300

    Add logos and update header layout with new images and styles

---
+++ src/lib/layout/header.svelte
@@ -0,0 +1,16 @@
+<script>
+	import logoMada from '$lib/assets/images/madagascar.webp';
+	import logoMESUPRES from '$lib/assets/images/mesupres.webp';
+</script>
+
+<header>
+	<nav>
+		<a href="/" aria-label="link to another website">
+			<img src={logoMada} alt="Logo Repoblikan'i Madagasikara" />
+		</a>
+		<span>&</span>
+		<a href="/" aria-label="link to another website">
+			<img src={logoMESUPRES} alt="Logo MESUPRES" />
+		</a>
+	</nav>
+</header>
